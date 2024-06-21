be26672bbf7206de5799ad6174c6149865f0d5f0
thevikramkumal <vikramkumar3502@gmail.com>
2024-06-21 10:25:56 +0530

    react file create

---
+++ basicsreact/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function App() {
+  return (
+    <><h1>hello react | my name is vikram i learn from chai aur code react proggam  learn about how to create rect and many more thing </h1></>
+    
+  );
+}
+
+export default App;
